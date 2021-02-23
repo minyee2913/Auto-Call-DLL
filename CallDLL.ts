1918a473794a1e195c86129f72f2b8cc3c1002bb
minyee2913 <72301973+minyee2913@users.noreply.github.com>
2021-02-23 21:39:55 +0900

    Update CallDLL.ts

--- CallDLL.ts
+++ CallDLL.ts
@@ -1,24 +1,18 @@
-import { readdir } from 'fs';
+import { readdir, readdirSync } from 'fs';
 import { NativeModule } from './bdsx';
 let localfile = './'
 let NOT = [
     "ChakraCore.dll",
     "Chakra.dll",
-    "bdghelp.dll"
+    "dbghelp.dll"
 ]
 
 
-readdir(localfile, function(error, filelist){
-    filelist.forEach((v)=>{
-        if (NOT.includes(v)) {
-            return;
-        }
-        if (v.includes(`.dll`)) console.log(`[CALL DLL] ${v}`);
-    });
-    filelist.forEach((v)=>{
-        if (NOT.includes(v)) {
-            return;
-        }
-        if (v.includes(`.dll`)) NativeModule.load(`${localfile}/${v}`);
-    })
+let dir = readdirSync(localfile);
+dir.forEach((v)=>{
+    if (NOT.includes(v)) {
+        return;
+    }
+    if (v.includes(`.dll`)) console.log(`[CALL DLL] ${v}`);
+    if (v.includes(`.dll`)) NativeModule.load(`${v}`);
 });
